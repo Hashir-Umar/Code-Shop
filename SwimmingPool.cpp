d13d79677e1c2fb56abaebebb4d0d51abc1a6e05
Hashir Umar <32617270+Hashir-Umar@users.noreply.github.com>
2018-03-23 22:52:36 +0500

    uploading first time

---
+++ SwimmingPool.cpp
@@ -0,0 +1,104 @@
+#include "SwimmingPool.h"
+
+SwimmingPool::SwimmingPool(double l, double w, double d, double RF, double RD, double CW)
+{
+	length = l;
+	width = w;
+	depth = d;
+	rateOfFilling = RF;
+	rateOfDraining = RD;
+	currntWaterInThePool = CW;
+}
+SwimmingPool::~SwimmingPool()
+{
+	length = 0.0;
+	width = 0.0;
+	depth = 0.0;
+	rateOfFilling = 0.0;
+	rateOfDraining = 0.0;
+    currntWaterInThePool = 0.0;
+}
+
+double SwimmingPool::getLength()
+{
+	return length;
+}
+double SwimmingPool::getWidth()
+{
+	return width;
+}
+double SwimmingPool::getDepth()
+{
+	return depth;
+}
+double SwimmingPool::getRateOfFilling()
+{
+	return rateOfFilling;
+}
+double SwimmingPool::getRateOfDraining()
+{
+	return rateOfDraining;
+}
+double SwimmingPool::getCurrentWaterLevel()
+{
+	return currntWaterInThePool;
+}
+
+void SwimmingPool::setLength(double l)
+{
+	length = l;
+}
+void SwimmingPool::setWidth(double w)
+{
+	width = w;
+}
+void SwimmingPool::setDepth(double d)
+{
+	depth = d;
+}
+void SwimmingPool::setRateOfFilling(double rf)
+{
+	rateOfFilling = rf;
+}
+void SwimmingPool::setRateOfDraining(double rd)
+{
+	rateOfDraining = rd;
+}
+void SwimmingPool::setCurrentWaterLevel(double c)
+{
+	currntWaterInThePool = c;
+}
+
+double SwimmingPool::getCapacityOfThePool()
+{
+	return (length * width * depth)*7.48052;
+}
+double SwimmingPool::getCapacityOfThePool(double filled)
+{
+	return getCapacityOfThePool() - filled;
+}
+double SwimmingPool::getTimeToFillThePool()
+{
+	return getCapacityOfThePool() / rateOfFilling;
+}
+double SwimmingPool::getTimeToDrainThePool()
+{
+	return currntWaterInThePool / rateOfDraining;
+}
+double SwimmingPool::addWaterInSpecificTime(double givinTime)
+{
+	double addedWater = rateOfFilling * givinTime;	//Stooring The Water In Another Variable for Later Use
+	currntWaterInThePool = currntWaterInThePool + (addedWater);
+    double poolCapacity = getCapacityOfThePool();
+	//In case Of Overflow This If statement Will be Executed
+    if (currntWaterInThePool > poolCapacity)    currntWaterInThePool = poolCapacity;
+	return currntWaterInThePool;
+}
+double SwimmingPool::drainWaterInSpecificTime(double givinTime)
+{
+	double drainedWater = rateOfDraining * givinTime;		//Stooring The Water In Another Variable for Later Use
+	currntWaterInThePool = currntWaterInThePool - (drainedWater);
+	//In case Of Underflow This If statement Will be Executed
+	if (currntWaterInThePool < 0)  currntWaterInThePool = 0;
+	return currntWaterInThePool;
+}
---
+++ main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include "SwimmingPool.h"
+
+using namespace std;
+
+int main()
+{
+	double l, w, d, fR, dR;
+	int choice;
+
+	cout << "Enter Details:- " << endl;
+	cout << "\n\tLength: ";
+	cin >> l;
+	cout << "\tWidth: ";
+	cin >> w;
+	cout << "\tDepth: ";
+	cin >> d;
+    cout << "\tFilling Rate: ";
+	cin >> fR;
+	cout << "\tDraining Rate: ";
+	cin >> dR;
+
+	SwimmingPool obj(l, w, d, fR, dR);
+
+	while (true)
+	{
+		system("cls");
+        
+		cout << "1) Get Current Water Level" << endl;
+		cout << "2) Get total capacity" << endl;
+		cout << "3) Add Water" << endl;
+		cout << "4) Drain Water" << endl;
+		cout << "5) Time to fill complete Pool?" << endl;
+		cout << "6) Time to Drain complete Pool?" << endl;
+		cout << "7) Exit" << endl;
+		cout << "\nPress: ";
+		cin >> choice;
+
+		system("cls");
+
+		if(choice == 1)
+			cout << obj.getCurrentWaterLevel() << endl;
+		else if (choice == 2)
+			cout << obj.getCapacityOfThePool() << endl;
+		else if (choice == 3)
+		{
+			cout << "For How Long(Time in minutes): ";
+			cin >> l;
+			obj.addWaterInSpecificTime(l);
+			cout << "New Water Level: " << obj.getCurrentWaterLevel() << endl;
+		}
+		else if (choice == 4)
+		{
+			cout << "For How Long(Time in minutes): ";
+			cin >> l;
+			obj.drainWaterInSpecificTime(l);
+			cout << "New Water Level: " << obj.getCurrentWaterLevel() << endl;
+		}
+		else if (choice == 5)
+			cout << "Pool Will be filled in: " << obj.getTimeToFillThePool() << " Minutes." << endl;
+		else if (choice == 6)
+			cout << "Pool Will be drained in: " << obj.getTimeToDrainThePool() << " Minutes." << endl;
+		else if (choice == 7)
+			return 0;
+
+		system("pause");
+	}
+
+	return 0;
+}
