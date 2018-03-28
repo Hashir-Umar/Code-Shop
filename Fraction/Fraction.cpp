35418938cc275a29bab21c0021e44149b8d49104
Hashir Umar <32617270+Hashir-Umar@users.noreply.github.com>
2018-03-28 21:04:02 +0500

    Uploading Fraction.cpp

---
+++ Fraction/Fraction.cpp
@@ -0,0 +1,52 @@
+#include "Fraction.h"
+
+
+void Fraction::display()
+{
+	cout << num << "/" << den << endl;
+}
+
+Fraction::Fraction(int n, int d)
+{
+	if (d==0)
+		d=1;
+
+	num = n;
+	den = d;
+}
+
+Fraction::Fraction(const Fraction &obj)
+{
+	num = obj.num;
+	den = obj.den;
+}
+
+Fraction::Fraction()
+{
+	num = 0;
+	den = 1;
+}
+
+Fraction::~Fraction()
+{
+	num = 0;
+	den = 1;
+}
+
+int Fraction::getNum() const
+{
+	return num;
+}
+int Fraction::getDen() const
+{
+	return den;
+}
+
+void Fraction::setNum(int n)
+{
+	num = n;
+}
+void Fraction::setDen(int d)
+{
+	den = d;
+}
